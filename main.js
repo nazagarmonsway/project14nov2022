0c02472a875d265bfaf34bbee915e2563278244f
nazagarmonsway <100462043+nazagarmonsway@users.noreply.github.com>
2022-11-28 15:47:54 +1300

    add elements to page via js

--- main.js
+++ main.js
@@ -1,51 +1,12 @@
-// document.querySelector("h1").style.color = "#0f0";
-// document.querySelector(".header").style.color = "#f33";
-// document.querySelector("#header").style.color = "#f99";
-// document.querySelector('[href="https://www.google.com"]').style.fontSize = "4em";
+function submit() {
+  var input = document.querySelector("[data-input]");
+  var value = input.value;
+  var ul = document.querySelector("[data-list]");
 
-// function myFirstFunction() {
-// document.body.style.backgroundColor = "#000"
-// }
+  var li = document.createElement("li");
 
-// // myFirstFunction()
+  li.textContent = value;
 
-// function simpleConsolePrint(){
-//     console.log("you just called a function yay!")
-// }
-
-// document.querySelector("#btn").addEventListener("click", simpleConsolePrint )
-// document.querySelector("#btn").addEventListener("dblclick", myFirstFunction)
-
-var h1 = document.createElement("h1");
-h1.innerHTML = "hello world";
-document.body.appendChild(h1);
-
-var p = document.createElement("p");
-p.textContent = "this paragraph is created in javascript";
-document.body.appendChild(p)
-
-function addElementToPage(){
-    var h3 = document.createElement("h3");
-    h3.textContent = "i am a text with some styling";
-    h3.style.color = "#f99";
-    h3.style.fontSize = "3em"
-    h3.style.border = "3px dashed"
-    document.body.appendChild(h3);
+  ul.appendChild(li);
+  document.querySelector("[data-input]").value = ""
 }
-
-function addImage(){
-    var image = document.createElement("img");
-    image.src="https://picsum.photos/id/1060/300"
-    image.style.border = "2px dotted"
-    document.querySelector("#image-holder").appendChild(image)
-
-}
-
-
-
-function setDarkMode(){
-    document.body.classList.toggle("dark-mode")
-    document.querySelector("#modes").innerHTML = "light mode"
-}
-
-
